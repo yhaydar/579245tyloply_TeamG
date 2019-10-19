fb91ac5020a9482979998d897bed93185c1484c1
fcoccimi <17952287+fcoccimi@users.noreply.github.com>
2019-10-19 19:35:28 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/bbqbuddy/MainActivity.java
+++ app/src/main/java/com/example/bbqbuddy/MainActivity.java
@@ -7,6 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 public class MainActivity extends AppCompatActivity {
 
     @Override
+    //Test
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);

1b6efc8c4df517aab8291fb2cefa15293668c2da
Greg <cao.gregory@gmail.com>
2019-10-31 08:15:40 -0400

    -connected firebase

--- app/src/main/java/backend/DatabaseController.java
+++ app/src/main/java/backend/DatabaseController.java
@@ -1,4 +1,30 @@
 package backend;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 public class DatabaseController {
+    private FirebaseDatabase database;
+    DatabaseReference reference;
+
+    public DatabaseController(String ref){
+       database = FirebaseDatabase.getInstance();
+       reference = database.getReference(ref);
+    }
+
+    public void setDatabaseRef(String ref){
+        reference = database.getReference(ref);
+    }
+
+    public void writeToDB(String ref, String message){
+        setDatabaseRef(ref);
+        reference.setValue(message);
+
+        //reference.addValueEventListener(new Value);
+
+    }
+
+    public void readFromDB(){
+        reference.child("Poultry");
+    }
 }
